d9c7acc03f4e650856c6f9e1a3e742c3480319df
fwcd <fwcdmail@gmail.com>
2018-06-22 00:02:59 +0200

    Added diatonic scale degrees

---
+++ src/main/java/com/fwcd/harmony/scale/DiatonicMajorDegree.java
@@ -0,0 +1,11 @@
+package com.fwcd.harmony.scale;
+
+public enum DiatonicMajorDegree {
+	TONIC,
+	SUPERTONIC,
+	MEDIANT,
+	SUBDOMINANT,
+	DOMINANT,
+	SUBMEDIANT,
+	LEADING_TONE
+}
---
+++ src/main/java/com/fwcd/harmony/scale/DiatonicNaturalMinorDegree.java
@@ -0,0 +1,11 @@
+package com.fwcd.harmony.scale;
+
+public enum DiatonicNaturalMinorDegree {
+	TONIC,
+	SUPERTONIC,
+	MEDIANT,
+	SUBDOMINANT,
+	DOMINANT,
+	SUBMEDIANT,
+	SUBTONIC
+}
